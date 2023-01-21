3720e818783714c7d48766cb2eae934ca20ed4f9
javilonte <javgofi@gmail.com>
2023-01-21 12:39:06 -0600

    Commented some code lines

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import notesRoutes from './routes/notes';
 import morgan from 'morgan';
+import createHttpError, { isHttpError} from "http-errors";
 
 const app = express();
 
@@ -12,16 +13,21 @@ app.use(express.json()); // Parse JSON bodies from us in POST requests
 app.use("/api/notes", notesRoutes);
 
 app.use((req,res,next) => {
-  next(Error('Endpoint not found'));
+  next(createHttpError(404, 'Endpoint not found'));
 });
 
 //eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = "An unknown error occurred!";
-  if (error instanceof Error)
+  let statusCode = 500;
+  // If the error is an instance of httpError class, it uses its values to get the error message and status code
+  if(isHttpError(error)){
+    statusCode = error.status;
     errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  }
+   // It stablish the status code and sends a JSON response with the error message 
+  res.status(statusCode).json({ error: errorMessage });
 })
 
 export default app;
