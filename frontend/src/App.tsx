cc24ebe733cd537eaafe152ae4638e5265b61d88
javilonte <javgofi@gmail.com>
2023-01-26 17:21:27 -0600

    Added Deleting button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import styles from './styles/NotePage.module.css';
 import stylesUtils from './styles/utils.module.css';
 import * as NotesApi from './network/notes_api';
 import { AddNoteDialog } from './components/AddNoteDialog';
+import {FaPlus} from 'react-icons/fa';
 
 function App() {
 
@@ -31,19 +32,31 @@ function App() {
     loadNotes();
   },[]);
 
+  async function deleteNote (note: NoteModel) {
+    try {
+      await NotesApi.deleteNote(note._id);
+      setNotes(notes.filter(existingNote => existingNote._id !== note._id));
+    } catch(error) {
+      console.log(error);
+      alert(error);
+    }
+  }
 
   return (
     <Container>
       <Button 
-      className={`mb-4 ${stylesUtils.blockCenter}` }
+      className={`mb-4 ${stylesUtils.blockCenter} ${stylesUtils.flexCenter}` }
       onClick={()=> setShowAddNoteDialog(true)}>
+        <FaPlus />
         Add New Note
       </Button>
       <Row xs={1} md={2} lg={3} className='g-4'>
         
       {notes.map(note => (
         <Col key={note._id}>
-        <Note note={note} className={styles.note} />
+        <Note note={note}
+          onDeleteNoteClicked={deleteNote}
+          className={styles.note} />
         </Col>
       ))}
       
