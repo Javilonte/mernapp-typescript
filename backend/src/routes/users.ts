29d35752c0ba57b394e688860daf60ab7267e6e9
javilonte <javgofi@gmail.com>
2023-02-04 16:39:20 -0600

    Create route for users auth

---
+++ backend/src/routes/users.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import * as UserController from '../controllers/users';
+
+
+const router = express.Router();
+
+router.post('/signup', UserController.singUp);
+
+export default router; 
