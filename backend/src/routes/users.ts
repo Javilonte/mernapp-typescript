279aae23394e52954150b8b15b8d839870b828ac
javilonte <javgofi@gmail.com>
2023-02-08 19:19:33 -0600

    set the routes for the authenticated user

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -5,6 +5,8 @@ import * as UserController from '../controllers/users';
 
 const router = express.Router();
 
+router.get('/authenticated', UserController.getAuthenticatedUser);
+
 router.post('/signup', UserController.singUp);
 
 router.post('/login', UserController.login);
