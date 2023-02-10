b21b194acc9d982c1e436dd4242bd83d48aa60fa
javilonte <javgofi@gmail.com>
2023-02-10 15:31:01 -0600

    shortening code inside addeditnotedialogtsx

--- frontend/src/components/AddEditNoteDialog.tsx
+++ frontend/src/components/AddEditNoteDialog.tsx
@@ -12,75 +12,72 @@ interface AddNoteDialogProps {
     onNoteSaved: (note: Note) => void,
 }
 
-export const AddEditNoteDialog = ({noteToEdit, onDismiss, onNoteSaved}: AddNoteDialogProps) => {
-  
-      const  {register, handleSubmit, formState: {errors, isSubmitting }} = useForm<NoteInput>({
+export const AddEditNoteDialog = ({ noteToEdit, onDismiss, onNoteSaved }: AddNoteDialogProps) => {
+
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<NoteInput>({
         defaultValues: {
             title: noteToEdit?.title || "",
             text: noteToEdit?.text || "",
 
         }
-      });
+    });
 
     async function onSubmit(input: NoteInput) {
         try {
             let noteResponse: Note;
-            if(noteToEdit) {
+            if (noteToEdit) {
                 noteResponse = await NotesApi.updateNote(noteToEdit._id, input);
             } else {
                 noteResponse = await NotesApi.createNote(input);
             }
             onNoteSaved(noteResponse);
             onDismiss();
-        } catch(error) {
+        } catch (error) {
             console.log(error);
             alert(error);
         }
     }
-  
-      return (
+
+    return (
 
 
-    <Modal show onHide={onDismiss}>
-        <Modal.Header closeButton>
-            <Modal.Title>{noteToEdit ? "Edit note " : "Add note"}</Modal.Title>
-        </Modal.Header>
+        <Modal show onHide={onDismiss}>
+            <Modal.Header closeButton>
+                <Modal.Title>{noteToEdit ? "Edit note " : "Add note"}</Modal.Title>
+            </Modal.Header>
 
-        <Modal.Body>
-            <Form id="addEditNoteForm" onSubmit={handleSubmit(onSubmit)}>
-                <TextInputField 
-                    name="title"
-                    label="Title"
-                    type="text"
-                    placeholder="Enter title"
-                    register={register}
-                    registerOptions={{required: "Required"}}
-                />
-                <Form.Group className="mb-3">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" placeholder="Enter title" isInvalid={!!errors.title} {...register("title", {required: "Required"})} />
-                </Form.Group>
-                <Form.Control.Feedback type="invalid">
-                    {errors.title?.message} // Tells you the error if title is undefined
-                </Form.Control.Feedback>
-                <Form.Group className="mb-3">
+            <Modal.Body>
+                <Form id="addEditNoteForm" onSubmit={handleSubmit(onSubmit)}>
+                    <TextInputField
+                        name="title"
+                        label="Title"
+                        type="text"
+                        placeholder="Enter title"
+                        register={register}
+                        registerOptions={{ required: "Required" }}
+                    />
+
+                    <TextInputField
+                        name="text"
+                        label="Text"
+                        as="textarea"
+                        placeholder="Enter text"
+                        rows={5}
+                        register={register}
+
+                    />
                     
-                <Form.Label>Text</Form.Label>
-                    <Form.Control as="textarea" 
-                    rows={5}
-                    placeholder="Enter your note" {...register("text")}/>
-                </Form.Group>
-            </Form>
-        </Modal.Body>
-        <Modal.Footer>
-            <Button 
-            type="submit"
-            form="addEditNoteForm"
-            disabled={isSubmitting}
-            >
-                Save Note
-            </Button>
-        </Modal.Footer>
-    </Modal>
-  )
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    type="submit"
+                    form="addEditNoteForm"
+                    disabled={isSubmitting}
+                >
+                    Save Note
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
 }
